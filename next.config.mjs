1b7d53ad11eafd7e7bf82d1fc567b5f0b8aea919
Muhammad Rehman <rehmanattock30@gmail.com>
2025-11-30 21:30:19 +0500

    fix bug

--- next.config.mjs
+++ next.config.mjs
@@ -18,26 +18,6 @@ const nextConfig = {
       },
     ],
   },
-  webpack: (config, { isServer, webpack }) => {
-    // Exclude problematic Windows system directories
-    if (config.snapshot) {
-      config.snapshot.managedPaths = [];
-    }
-    
-    // Add ignore patterns for Windows
-    if (config.watchOptions) {
-      config.watchOptions.ignored = [
-        /node_modules/,
-        /\.next/,
-        /\.git/,
-      ];
-    }
-
-    // Disable filesystem caching to avoid Windows permission issues
-    config.cache = false;
-    
-    return config;
-  },
 };
 
 export default nextConfig;
